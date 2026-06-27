08f71bad806a31cb63fd2ea62d2b902708772e9a
Imaven1 <158128049+Imaven1@users.noreply.github.com>
2026-06-27 21:07:14 +0800

    removed inherited methods from scheduler in fcfsscheduler class

--- FCFSScheduler.h
+++ FCFSScheduler.h
@@ -11,68 +11,17 @@
 
 #include "Process.h"
 #include "Config.h"
+#include "Scheduler.h"
 
 class ProcessRegistry;
 struct AppState;
 
-class FCFSScheduler {
-private:
-	enum class ProcessState {
-		Waiting,
-		Running,
-		Finished
-	};
-
-	struct ProcessSnapshot {
-		std::shared_ptr<Process> process;
-		ProcessState state;
-		int coreId;
-		std::string stateTimestamp;
-	};
-
-	struct CoreWorker {
-		std::thread thread;
-		std::mutex mutex;
-		std::condition_variable cv;
-		std::shared_ptr<Process> assignedProcess;
-		bool hasWork = false;
-		bool stopRequested = false;
-	};
-
-	int numCores;
-	std::deque<std::shared_ptr<Process>> readyQueue;
-	std::vector<std::unique_ptr<CoreWorker>> workers;
-	std::thread schedulerThread;
-	std::thread tickThread;
-	std::mutex queueMutex;
-	std::condition_variable queueCv;
-	mutable std::mutex stateMutex;
-	std::map<int, ProcessSnapshot> snapshots;
-	bool acceptingWork;
-	bool stopRequested;
-	ProcessRegistry* registry = nullptr;
-	AppState* appState = nullptr;
-	bool batchGenerationEnabled = false;
-	int nextGeneratedProcessIndex = 1;
-	long long lastBatchSpawnTick = 0;
-
-	void schedulerLoop();
-	void workerLoop(int coreId);
-	void tickLoop();
-	void maybeSpawnBatchProcess(long long currentTick);
-	void updateProcessState(const std::shared_ptr<Process>& process, ProcessState state, int coreId);
-	static std::string buildTimestamp();
+class FCFSScheduler : public Scheduler {
 
 public:
-	explicit FCFSScheduler(int cores);
-	~FCFSScheduler();
+	explicit FCFSScheduler(int cores) ;
+	~FCFSScheduler() override;
 
-	void setRegistry(ProcessRegistry* reg);
-	void setAppState(AppState* state);
-	void setBatchGenerationEnabled(bool enabled);
-	void addProcess(const std::shared_ptr<Process>& process);
-	void start();
-	void stop();
-	bool isFinished() const;
-	std::string screenLs() const;
+protected:
+	void workerLoop(int coreId) override;
 };
