ab2b0da1eaf02cc13523655b6e86e1b1b1ff378c
Imaven1 <158128049+Imaven1@users.noreply.github.com>
2026-07-10 17:51:35 +0800

    FlatMemoryAllocator private fields

--- FlatMemoryAllocator.h
+++ FlatMemoryAllocator.h
@@ -10,4 +10,14 @@ public:
 	void* allocate(size_t size) override;
 	void deallocate(void* ptr) override;
 	std::string visualizeMemory() override;
+
+private:
+	size_t maximumSize;
+	size_t allocatedSize;
+	std::vector<char>memory;
+	std::fill(allocationMap.begin(), allocationMap.end(), false);
+
+	bool canAlocateAt(size_t index, size_t size) const;
+	void allocateAt(size_t index, size_t size);
+	void deallocateAt(size_t index);
 }
